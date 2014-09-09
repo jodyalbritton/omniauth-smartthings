85ee62e403da646f1a2b51b5a50ea011d8511f52
Jody Albritton <jody.albritton@gmail.com>
2014-09-09 17:20:14 -0500

    fix

--- lib/omniauth/strategies/smartthings.rb
+++ lib/omniauth/strategies/smartthings.rb
@@ -19,9 +19,6 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class SmartThings < OmniAuth::Strategies::OAuth2
-
-      option :name, "smartthings"
-
       option :client_options, {
         :site => 'https://graph.api.smartthings.com',
         :authorize_url => '/oauth/authorize',
